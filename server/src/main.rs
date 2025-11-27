c9565514299526117fec5e1a9b973090f2f4a2a3
Skulhunter5 <linus.huster@gmail.com>
2025-11-27 16:24:17 +0100

    Progress transfer

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,27 +1,36 @@
 use std::{
+    collections::HashMap,
     fs::{self, DirBuilder},
     path::Path,
+    sync::Arc,
 };
 
-use base64::Engine as _;
-use rand::RngCore;
-use rocket::{fs::NamedFile, routes};
+use argon2::{
+    Argon2, PasswordHasher,
+    password_hash::{SaltString, rand_core::OsRng},
+};
+use rocket::{
+    State, async_trait,
+    form::{Form, FromForm},
+    fs::NamedFile,
+    http::CookieJar,
+    response::Redirect,
+    routes,
+    tokio::sync::RwLock,
+};
 use rocket_dyn_templates::Template;
 use serde::Serialize;
 
 pub const RUN_DIR: &'static str = "./run";
 pub const SALT_FILE: &'static str = "./run/salt";
 
-fn load_or_generate_salt() -> String {
+fn load_or_generate_salt() -> SaltString {
     if let Ok(salt) = fs::read_to_string(&SALT_FILE) {
-        return salt;
+        return SaltString::from_b64(&salt).expect("failed to create salt");
     }
 
-    let mut rng = rand::rng();
-    let mut salt = [0u8; 16];
-    rng.fill_bytes(&mut salt);
-    let salt = base64::engine::general_purpose::STANDARD.encode(&salt);
-    fs::write(&SALT_FILE, &salt).expect("failed to write salt to file");
+    let salt = SaltString::generate(&mut OsRng);
+    fs::write(&SALT_FILE, salt.as_str()).expect("failed to write salt to file");
     return salt;
 }
 
@@ -57,9 +66,110 @@ async fn index() -> Template {
     )
 }
 
+#[async_trait]
+trait UserService: Send + Sync {
+    async fn verify_password(&self, username: &str, password: &str) -> bool;
+    async fn add_user(&self, username: &str, password: &str) -> bool;
+}
+
+pub struct InMemoryUserService {
+    users: RwLock<HashMap<String, String>>,
+    salt: SaltString,
+}
+
+impl InMemoryUserService {
+    pub fn new(salt: SaltString) -> Self {
+        Self {
+            users: RwLock::new(HashMap::new()),
+            salt,
+        }
+    }
+
+    fn hash_password(&self, password: &str) -> String {
+        Argon2::default()
+            .hash_password(password.as_bytes(), self.salt.as_salt())
+            .unwrap()
+            .to_string()
+    }
+}
+
+#[async_trait]
+impl UserService for InMemoryUserService {
+    async fn verify_password(&self, username: &str, password: &str) -> bool {
+        if let Some(correct_hash) = self.users.read().await.get(username) {
+            let hash = self.hash_password(password);
+            return hash == *correct_hash;
+        }
+        return false;
+    }
+
+    async fn add_user(&self, username: &str, password: &str) -> bool {
+        if self.users.read().await.contains_key(username) {
+            return false;
+        }
+
+        let hash = self.hash_password(password);
+        self.users.write().await.insert(username.to_owned(), hash);
+        return true;
+    }
+}
+
+#[derive(FromForm)]
+struct SignupForm<'a> {
+    username: &'a str,
+    password: &'a str,
+}
+
+#[rocket::post("/signup", data = "<form>")]
+async fn signup(
+    form: Form<SignupForm<'_>>,
+    user_service: &State<Arc<dyn UserService>>,
+) -> Redirect {
+    if user_service.add_user(form.username, form.password).await {
+        todo!("successful signup");
+    } else {
+        todo!("failed to sign up");
+    }
+}
+
+#[derive(FromForm)]
+struct LoginForm<'a> {
+    username: &'a str,
+    password: &'a str,
+}
+
+#[rocket::post("/login", data = "<form>")]
+async fn login(
+    form: Form<LoginForm<'_>>,
+    _jar: &CookieJar<'_>,
+    user_service: &State<Arc<dyn UserService>>,
+) -> Redirect {
+    if user_service
+        .verify_password(&form.username, &form.password)
+        .await
+    {
+        todo!();
+        Redirect::to("/")
+    } else {
+        Redirect::to("/login?error")
+    }
+}
+
 #[rocket::get("/about")]
 async fn about() -> Option<NamedFile> {
     let path = Path::new("static").join("about.html");
+    NamedFile::open(path).await.ok()
+}
+
+#[rocket::get("/signup")]
+async fn signup_page() -> Option<NamedFile> {
+    let path = Path::new("static").join("signup.html");
+    NamedFile::open(path).await.ok()
+}
+
+#[rocket::get("/login")]
+async fn login_page() -> Option<NamedFile> {
+    let path = Path::new("static").join("login.html");
     NamedFile::open(path).await.ok()
 }
 
@@ -69,14 +179,18 @@ async fn main() -> Result<(), rocket::Error> {
         .recursive(true)
         .create(&RUN_DIR)
         .expect("failed to create run dir");
-    let _salt = load_or_generate_salt();
+    let salt = load_or_generate_salt();
 
     // let rocket_config = rocket::Config::figment().merge(("template_dir", "static/"));
     // rocket::custom(&rocket_config)
 
     rocket::build()
-        .mount("/", routes![index, about])
+        .mount(
+            "/",
+            routes![index, about, login_page, login, signup_page, signup],
+        )
         .attach(Template::fairing())
+        .manage(Arc::new(InMemoryUserService::new(salt)) as Arc<dyn UserService>)
         .launch()
         .await?;
 
